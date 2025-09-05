c295d583aab4c7398be6c89dad7a736fa179d92b
LeandroUTNFRA <leandroes2403@gmail.com>
2025-09-05 09:48:59 -0300

    quien-soy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)
+    },
+    {
+        path: 'login', loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent)
+    },
+    {
+        path: 'about', loadComponent: () => import('./components/quien-soy/quien-soy.component').then(m => m.QuienSoyComponent)
+    }
+];
